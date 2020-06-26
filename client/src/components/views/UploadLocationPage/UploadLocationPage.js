bc245a93ef908ecd71ad68c3f1d5c094e8f0e3b9
joshfagen <j.fagen@muxlab.com>
2020-06-26 02:31:11 -0400

    initializes upload form

--- client/src/components/views/UploadLocationPage/UploadLocationPage.js
+++ client/src/components/views/UploadLocationPage/UploadLocationPage.js
@@ -2,8 +2,59 @@ import React from 'react'
 
 function UploadLocationPage() {
     return (
-        <div>
-            
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
+                <h2>Upload Location</h2>
+            </div>
+
+            <form onSubmit>
+                { /* Drop Zone */}
+
+                <br />
+                <br />
+                
+                <label>Title</label>
+                <input 
+                    onChange
+                    value
+                />
+
+                <br />
+                <br />
+                
+                <label>Description</label>
+                <textarea
+                    onChange
+                    value
+                />
+
+                <br />
+                <br />
+                
+                <label>Price ($)</label>
+                <input 
+                    onChange
+                    value
+                    type="number"
+                />
+
+                <select>
+                    
+                    <option key="" value="">
+                    
+                    </option>
+
+                </select>
+
+                <br />
+                <br />
+
+                <button 
+                    onClick
+                >
+                    Submit
+                </button>
+            </form>
         </div>
     )
 }
